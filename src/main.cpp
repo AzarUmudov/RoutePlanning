da3e8914c2facec30727a54f2270aa0f87e24b24
AzarUmudov <azarumudov@gmail.com>
2020-05-17 22:28:29 +0400

    Test folder deleted and modified readme.md

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,7 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+   
 	float start_x, start_y, end_x, end_y;
 	std::cout << "Enter start x btw. 0 - 99: ";
 	std::cin >> start_x;
@@ -65,16 +62,13 @@ int main(int argc, const char **argv)
 	std::cout << "Enter end y btw. 0 - 99: ";
 	std::cin >> end_y;
 
-    // Build Model.
     RouteModel model{osm_data};
 
-    // Create RoutePlanner object and perform A* search.
     RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
 
-    // Render results of search.
     Render render{model};
 
     auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
